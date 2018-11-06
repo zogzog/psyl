291aaee7a6d809eda2f8bcec4a0c7cade17f2dac
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2018-11-06 18:22:03 +0100

    test: remove unused operators

--- test/test_expressions.py
+++ test/test_expressions.py
@@ -9,16 +9,8 @@ from psyl.lisp import evaluate, GLOBALENV, parse
 def test_things():
     GLOBALENV.update({
         '+': op.add,
-        '-': op.sub,
         '*': op.mul,
-        '/': op.truediv,
         'today': date.today,
-        'monthstart': lambda dt: dt.replace(day=1),
-        'yearstart': lambda dt: dt.replace(day=1, month=1),
-        'yearend': lambda dt: dt.replace(day=31, month=12),
-        'deltadays': lambda dt, days: dt + relativedelta(days=days),
-        'deltamonths': lambda dt, months: dt + relativedelta(months=months),
-        'deltayears': lambda dt, years: dt + relativedelta(years=years)
     })
 
     assert evaluate('(+ 3 5)') == 8
