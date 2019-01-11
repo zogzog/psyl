d2c82dfffa3d0ec552b99bed4c1262d485f987c8
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2019-01-11 18:40:42 +0100

    pkg: fix desc

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='psyl',
       version='0.1.0',
       author='Pythonian',
       author_email='aurelien.campeas@pythonian.fr',
-      description='...',
+      description='a stripped down scheme-like parser',
 
       packages=['psyl'],
       install_requires=[
